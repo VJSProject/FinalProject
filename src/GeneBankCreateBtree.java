82041d6d3131969a60f89bf1c7ed4b63284a8a34
vmcerda <v_ezlife@yahoo.com>
2019-11-11 11:34:43 -0700

    Added Scanner to read file.  Issue if there are two lines of DNA(needs looked at)

--- src/GeneBankCreateBtree.java
+++ src/GeneBankCreateBtree.java
@@ -1,17 +1,55 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Scanner;
 
 public class GeneBankCreateBtree {
 
-    public static void main(String[] args){
+    private static String dnaSequence;
+
+    public static void main(String[] args) throws FileNotFoundException {
 
         if(args.length < 4){
             help();
         }
+
         if(Integer.parseInt(args[0]) > 1){
             System.out.println("Incorrect Usage( 0 or 1 required)");
             help();
         }
-        System.out.println("here");
+
+        if(args[1].equals('0')){
+//            If degree is zero, we need to find degree
+        }
+// reading file to parse DNA code//issue when there are two lines of DNA
+        try {
+            boolean dnaSection = false;
+            Scanner sc = new Scanner(new File(args[2]));
+            while(sc.hasNext()){
+                if(sc.nextLine().contains("ORIGIN")){
+                    dnaSection = true;
+                    while(dnaSection){
+                        String line = sc.nextLine();
+                        if(line.contains("//")){
+                            dnaSection =false;
+                        }else{
+                            System.out.println(line);
+                        }
+                    }
+                }
+//                if(sc.nextLine() == "ORIGIN"){
+//                    dnaSequence = sc.nextLine();
+//                    System.out.println(dnaSequence);
+//                    System.exit(0);
+//                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     private static void help() {
--- src/TreeObject.java
+++ src/TreeObject.java
@@ -1,2 +1,17 @@
-public class TreeObject {
+public class TreeObject<T> {
+
+    private T key;
+    private int frequency;
+
+    public TreeObject(T key){
+        this.key = key;
+    }
+
+    public T getKey(){
+        return this.key;
+    }
+
+    public void incFrequency(){
+        frequency++;
+    }
 }
