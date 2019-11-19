130f2eca88923009edf52236e13fbe806b434558
vmcerda <v_ezlife@yahoo.com>
2019-11-18 18:28:41 -0700

    Fixed the index out of bounds issue.  Also, added logic if there are two or more DNA values in the text file.

--- src/GeneBankCreateBtree.java
+++ src/GeneBankCreateBtree.java
@@ -26,9 +26,16 @@ public class GeneBankCreateBtree {
         }
 // reading file to parse DNA code
         dnaString = createLongString(args[2]);
-        System.out.println(dnaString);
-        createKeyValues(dnaString, args[1]);
-
+        if(dnaString.lines().count() > 1){
+            String[] lines = dnaString.split("\n");
+            for(String line : lines){
+                //need to pass each line into as Btree
+                ArrayList<String> test = createKeyValues(line,args[1]);
+                System.out.println(test);
+            }
+        }else {
+            createKeyValues(dnaString, args[1]);
+        }
     }
 
     private static String createLongString(String filename) {
@@ -39,8 +46,8 @@ public class GeneBankCreateBtree {
             Scanner sc = new Scanner(new File(filename));
             while(sc.hasNext()){
                 if(sc.nextLine().contains("ORIGIN")){
-                    bigString += "DNA" + i + " ";
-                    i++;
+//                    bigString += "DNA" + i + " ";
+//                    i++;
                     dnaSection = true;
                     while(dnaSection){
                         String line = sc.nextLine();
@@ -61,20 +68,22 @@ public class GeneBankCreateBtree {
         return bigString;
     }
 
-    //takes dna string and seperates it into substrings the size of arg "key"
+    //takes dna string and separates it into substrings the size of arg "key"
     //those substrings are then converted into binary
     private static ArrayList<String> createKeyValues(String dnaString, String key) {
     	ArrayList<String> tokens = new ArrayList<String>();
     	for(int i = 0; i< dnaString.length(); i++) {
     		int size = Integer.parseInt(key);
-    		String dnaSubstring = dnaString.substring(i,i+size);
-    		if(!dnaSubstring.contains("n")) {
-    			String hold = "";
-    			hold = dnaSubstring.replaceAll("[aA]", "00");
-    			hold = hold.replaceAll("[cC]", "01");
-    			hold = hold.replaceAll("[gG]", "10");
-    			hold = hold.replaceAll("[tT]", "11");
-    			tokens.add(hold);
+    		if(i+size < dnaString.length()){
+    		    String dnaSubstring = dnaString.substring(i, i + size);
+    		    if(!dnaSubstring.contains("n")) {
+                    String hold = "";
+                    hold = dnaSubstring.replaceAll("[aA]", "00");
+                    hold = hold.replaceAll("[cC]", "01");
+                    hold = hold.replaceAll("[gG]", "10");
+                    hold = hold.replaceAll("[tT]", "11");
+                    tokens.add(hold);
+                }
     		}
     	}
         return tokens;
