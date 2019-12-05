7368ffff0097f91bbc37ad86e8415696c061f174
John Capone <jac10598@gmail.com>
2019-12-05 16:53:06 -0600

    Added more arguements checks to GeneBankSearch.java

--- src/GeneBankSearch.java
+++ src/GeneBankSearch.java
@@ -8,10 +8,24 @@ public class GeneBankSearch {
         if(args.length > 5 || args.length < 3 || Integer.parseInt(args[0]) > 1){
             help();
         }
-
+        
+        //all arguements given
         if(args.length == 5){
             debugLevel = Integer.parseInt(args[4]);
+            cacheSize = Integer.parseInt(args[3]);
         }
+        
+        //some arguements omitted
+        if(args.length == 4){
+            if(Integer.parseInt(args[0])==0){ //no cache
+                 debugLevel = Integer.parseInt(args[3]);
+            }
+            if(Integer.parseInt(args[0])==1){ //yes cache
+                 cacheSize = Integer.parseInt(args[3]);
+                 debugLevel = 0;
+            }
+        }
+
 
         btreeFile = args[1];
         queryFile = args[2];
